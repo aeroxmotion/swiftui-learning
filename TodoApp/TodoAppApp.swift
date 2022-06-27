4fbe1ae62d0577f675c22c1f8b35ab022fc27e00
Camilo Rodriguez <aeroxmotion@hotmail.com>
2022-06-27 18:26:50 -0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Camilo Rodriguez on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

a123cd6482b32dfbcd7c118c556e12f1719aa6b3
Camilo Rodriguez <aeroxmotion@hotmail.com>
2022-06-27 20:17:55 -0500

    Missin file

---
+++ TodoApp/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by Camilo Rodriguez on 27/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13 Pro")
+    }
+}

5ffa8495af11f90c98cea5ea3df3a82c8311ad86
Camilo Rodriguez <aeroxmotion@hotmail.com>
2022-06-27 21:54:49 -0500

    Finished lesson: https://developer.apple.com/tutorials/swiftui/drawing-paths-and-shapes

---
+++ TodoApp/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  TodoApp
+//
+//  Created by Camilo Rodriguez on 27/06/22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
